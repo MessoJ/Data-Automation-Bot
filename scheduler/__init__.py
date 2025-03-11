00d8990aa6fe78e24d8e2b0913ca0db254745075
Meso Francis <118773373+MessoJ@users.noreply.github.com>
2025-03-11 15:54:16 +0300

    Create __init__.py

---
+++ scheduler/__init__.py
@@ -0,0 +1,7 @@
+"""
+Scheduler module for the data automation bot.
+This module handles scheduling and executing automated data processing jobs.
+"""
+from .job_scheduler import JobScheduler
+
+__all__ = ['JobScheduler']
